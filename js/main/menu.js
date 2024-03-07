bcbaa1b58794f6366f96b3876a4b5aa9764f1bee
AndreyNesterenko2020 <74727724+AndreyNesterenko2020@users.noreply.github.com>
2024-03-07 12:55:05 -0500

    Update menu.js

--- js/main/menu.js
+++ js/main/menu.js
@@ -4,7 +4,7 @@ menuSystem.level = -1;
 menuSystem.lastSave = -1;
 menuSystem.levelJustBeaten = -1;
 menuSystem.allowSwitchItems = false;
-menuSystem.gameVersion = "DEV";
+menuSystem.gameVersion = "1.0";
 
 menuSystem.init = function() {
   menuSystem.mainMenu = document.createElement("div");
